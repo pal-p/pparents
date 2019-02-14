fbac413e2c70662fae3e5dd76945570e883c3627
Palwasha Pordeli <palwashap04@gmail.com>
2019-02-14 19:06:05 +1100

    initial routes setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dbURI = require("./config/keys").MONGOURI;
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 
 //connect to mongodb
 mongoose
@@ -10,5 +13,10 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hi there!"));
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("app is at port ${port}"));
