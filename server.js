6adacd4352b983b20ca26d98377f2343b766eec5
Palwasha Pordeli <palwashap04@gmail.com>
2019-02-14 18:30:34 +1100

    db connection setup

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dbURI = require("./config/keys").MONGOURI;
+
+//connect to mongodb
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("connected to db"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hi there!"));
 const port = process.env.PORT || 5000;
