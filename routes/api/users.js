2df24bd830c2da3dc3b353ea26395619248d397f
Palwasha Pordeli <palwashap04@gmail.com>
2019-02-17 12:48:24 +1100

    user created in db, password hashed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const router = express.Router();
-
+const gravatar = require("gravatar");
+const mongoose = require("mongoose");
+require("../../models/User");
+const User = mongoose.model("users");
+const bcrypt = require("bcryptjs");
 router.get("/test", (req, res) => res.json({ msg: "users is working" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const picture = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", // rating
+        d: "mm" // default to user icon pic
+      });
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        picture
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          if (err) throw err;
+          user.password = hash;
+          user
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dbURI = require("./config/keys").MONGOURI;
-const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const users = require("./routes/api/users");
 
 //connect to mongodb
 mongoose
@@ -19,4 +24,4 @@ app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("app is at port ${port}"));
+app.listen(port, () => console.log(`app is at port ${port}`));
