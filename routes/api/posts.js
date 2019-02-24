e60ee26b0c9d3990296ae598c099193c56828a4f
Palwasha Pordeli <palwashap04@gmail.com>
2019-02-24 18:14:59 +1100

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,24 @@ const validatePost = require("../../validation/post");
 
 router.get("/test", (req, res) => res.json({ msg: "posts is working" }));
 
+//delete post by id
+router.delete(
+  "/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.post_id)
+      .then(post => {
+        // Check if the login user is the owner of the post
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ authErr: "Not authorised" });
+        }
+        post.remove().then(() => res.json({ postDeleted: true }));
+      })
+      .catch(err =>
+        res.status(404).json({ postErr: "No posts exists with this id" })
+      );
+  }
+);
 //get all postsby descending date order
 router.get("/", (req, res) => {
   Post.find()
